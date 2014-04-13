618e16f65dd9bfd2cc7bc1a83cda30178f4c23a6
Bart Van Itterbeeck <bartvanitterbeeck@gmail.com>
2014-04-13 19:16:44 +0200

    850 px canvas

--- public/js/play.js
+++ public/js/play.js
@@ -1,62 +1,78 @@
-$(function(){
-	$('.imgurImg').each(function(){
-		src = $(this).attr('data-src');
-		thumb = imgurThumb(src, 'b');
-		$(this).attr('src',thumb);
-	})
+$(function() {
+    $('.imgurImg').each(function() {
+        src = $(this).attr('data-src');
+        thumb = imgurThumb(src, 'b');
+        $(this).attr('src', thumb);
+    })
 
-	var jsonHTML = $('#place-json').text();
-	var locals = jsonHTML.replace(/&quot;/g,'"');
-	locals = JSON.parse(locals);
-	console.log(locals.properties.people);
+    var jsonHTML = $('#place-json').text();
+    var locals = jsonHTML.replace(/&quot;/g, '"');
+    locals = JSON.parse(locals);
+    console.log(locals.properties.people);
 
-	for (var j = 0; j < locals.properties.people.length; j++) {
-		localProps = locals.properties.people[j];
-		console.log(localProps);
+    for (var j = 0; j < locals.properties.people.length; j++) {
+        localProps = locals.properties.people[j];
+        console.log(localProps);
 
-		switch(localProps.name.toLowerCase()){
-			case 'sex':
-				for (var i = 0; i < localProps.possibilities.length; i++) {
-					$('.questions').append('Is it '+localProps.possibilities[i]+'?<br>')
-				};
-				break;
-			case "glasses":
-				// for (var i = 0; i < localProps.possibilities.length; i++) {
-					$('.questions').append('Does it wear '+'glasses'+'?<br>')
-				// };
-				break;
-			case 'facial hair':
-				for (var i = 0; i < localProps.possibilities.length; i++) {
-					$('.questions').append('Does he have a '+localProps.possibilities[i]+'?<br>')
-				};
-				break;
-			case 'hair color':
-				for (var i = 0; i < localProps.possibilities.length; i++) {
-					$('.questions').append('Does it have  '+localProps.possibilities[i]+' Hair?<br>')
-				};
-				break;
-			case 'hair type':
-				for (var i = 0; i < localProps.possibilities.length; i++) {
-					$('.questions').append('Does it have  '+localProps.possibilities[i]+' Hair?<br>')
-				};
-				break;
-			case 'accesories':
-				for (var i = 0; i < localProps.possibilities.length; i++) {
-					$('.questions').append('Does it accesorize with '+localProps.possibilities[i]+'?<br>')
-				};
-				break;
-			case 'eye color':
-				for (var i = 0; i < localProps.possibilities.length; i++) {
-					$('.questions').append('Does it have '+localProps.possibilities[i]+' eyes?<br>')
-				};
-				break;
-			case 'skin color':
-				for (var i = 0; i < localProps.possibilities.length; i++) {
-					$('.questions').append('Is it '+localProps.possibilities[i]+' ?<br>')
-				};
-				break;
-		}
-	};
+        switch (localProps.name.toLowerCase()) {
+            case 'sex':
+                for (var i = 0; i < localProps.possibilities.length; i++) {
+                    $('.questions').append('<div class="quest">Is it ' + localProps.possibilities[i] + '?</div>')
+                };
+                break;
+            case "glasses":
+                // for (var i = 0; i < localProps.possibilities.length; i++) {
+                $('.questions').append('<div class="quest">Does it wear ' + 'glasses' + '?</div>')
+                // };
+                break;
+            case 'facial hair':
+                for (var i = 0; i < localProps.possibilities.length; i++) {
+                    $('.questions').append('<div class="quest">Does he have a ' + localProps.possibilities[i] + '?</div>')
+                };
+                break;
+            case 'hair color':
+                for (var i = 0; i < localProps.possibilities.length; i++) {
+                    $('.questions').append('<div class="quest">Does it have  ' + localProps.possibilities[i] + ' Hair?</div>')
+                };
+                break;
+            case 'hair type':
+                for (var i = 0; i < localProps.possibilities.length; i++) {
+                    $('.questions').append('<div class="quest">Does it have  ' + localProps.possibilities[i] + ' Hair?</div>')
+                };
+                break;
+            case 'accesories':
+                for (var i = 0; i < localProps.possibilities.length; i++) {
+                    $('.questions').append('<div class="quest">Does it accesorize with ' + localProps.possibilities[i] + '?</div>')
+                };
+                break;
+            case 'eye color':
+                for (var i = 0; i < localProps.possibilities.length; i++) {
+                    $('.questions').append('<div class="quest">Does it have ' + localProps.possibilities[i] + ' eyes?</div>')
+                };
+                break;
+            case 'skin color':
+                for (var i = 0; i < localProps.possibilities.length; i++) {
+                    $('.questions').append('<div class="quest">Is it ' + localProps.possibilities[i] + ' ?</div>')
+                };
+                break;
+        }
+    };
+
+    var qArr = [];
+
+    $('.quest').each(function() {
+        console.log($(this).html());
+        qArr.push($(this).html());
+    });
+
+    $('.questions .quest').empty();
+
+    console.log(qArr);
+
+    for (var i = 0; i < qArr.length; i++) {
+        $('select.q').append('<option>' + qArr[i] + '</option>');
+    };
+
 })
 
 
@@ -64,8 +80,8 @@ $(function(){
 For getting thumbnails from imgur
 */
 
-function imgurThumb(imgurl, size){
-/*
+function imgurThumb(imgurl, size) {
+    /*
 size types:
 s   Small Square    90x90   No
 b   Big Square  160x160 No
